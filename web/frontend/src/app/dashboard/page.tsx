513e51d64d2dc5de7ef2fcb4ae98486a31c2880b
Vladyslav Len <len.vladislavs@gmail.com>
2025-10-02 23:02:50 +0200

    chore(): added frontend

---
+++ web/frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import data from './data.json'
+import { ChartAreaInteractive } from '@/components/chart-area-interactive'
+import { DataTable } from '@/components/data-table'
+import { Navbar } from '@/components/navbar'
+
+export default function DashboardPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-1 flex-col pl-[20%] pr-[20%]">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="px-4 lg:px-6">
+              <ChartAreaInteractive />
+            </div>
+            <DataTable data={data} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
