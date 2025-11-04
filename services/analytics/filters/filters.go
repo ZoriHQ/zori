eba337f4579ecf7a90e4346bea3a92c884dac4aa
Vladyslav Len <len.vladislavs@gmail.com>
2025-11-04 20:44:00 +0100

    fix(): added missing tags on filters

--- services/analytics/filters/filters.go
+++ services/analytics/filters/filters.go
@@ -17,20 +17,20 @@ const (
 )
 
 type VisitorProfileFilter struct {
-	ProjectID  string  `query:"project_id" validate:"required" example:"proj_123"`
-	VisitorID  *string `query:"visitor_id" validate:"omitempty" example:"visitor_abc123"`
-	CustomerID *string `query:"customer_id" validate:"omitempty" example:"cus_xyz789"`
+	ProjectID  string  `json:"project_id" query:"project_id" form:"project_id" validate:"required" example:"proj_123"`
+	VisitorID  *string `json:"visitor_id" query:"visitor_id" form:"visitor_id" validate:"omitempty" example:"visitor_abc123"`
+	CustomerID *string `json:"customer_id" query:"customer_id" form:"customer_id" validate:"omitempty" example:"cus_xyz789"`
 }
 
 type SectionFilter struct {
-	ProjectID      string         `query:"project_id" validate:"required" example:"proj_123"`
-	TimeBoundaries TimeBoundaries `query:"time_range" validate:"required,oneof=last_hour today yesterday last_7_days last_30_days last_90_days" enums:"last_hour,today,yesterday,last_7_days,last_30_days,last_90_days" default:"last_7_days" example:"last_7_days"`
-	Referrer       *string        `query:"referrer" validate:"omitempty" example:"https://google.com"`
-	VisitorID      *string        `query:"visitor_id" validate:"omitempty" example:"visitor_abc123"`
-	CustomerID     *string        `query:"customer_id" validate:"omitempty" example:"cus_xyz789"`
+	ProjectID      string         `json:"project_id" query:"project_id" form:"project_id" validate:"required" example:"proj_123"`
+	TimeBoundaries TimeBoundaries `json:"time_range" query:"time_range" form:"time_range" validate:"required,oneof=last_hour today yesterday last_7_days last_30_days last_90_days" enums:"last_hour,today,yesterday,last_7_days,last_30_days,last_90_days" default:"last_7_days" example:"last_7_days"`
+	Referrer       *string        `json:"referrer" query:"referrer" form:"referrer" validate:"omitempty" example:"https://google.com"`
+	VisitorID      *string        `json:"visitor_id" query:"visitor_id" form:"visitor_id" validate:"omitempty" example:"visitor_abc123"`
+	CustomerID     *string        `json:"customer_id" query:"customer_id" form:"customer_id" validate:"omitempty" example:"cus_xyz789"`
 
-	Limit  int `query:"limit" validate:"omitempty,min=1,max=100" default:"50" minimum:"1" maximum:"100" example:"50"`
-	Offset int `query:"offset" validate:"omitempty,min=0" default:"0" minimum:"0" example:"0"`
+	Limit  int `json:"limit" query:"limit" form:"limit" validate:"omitempty,min=1,max=100" default:"50" minimum:"1" maximum:"100" example:"50"`
+	Offset int `json:"offset" query:"offset" form:"offset" validate:"omitempty,min=0" default:"0" minimum:"0" example:"0"`
 
 	UTMFilter
 	// TimeRange is a computed field
