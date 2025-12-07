345b51e34a289fed1ab38fe001990b678f3b2d82
Vladyslav Len <len.vladislavs@gmail.com>
2025-12-07 13:43:36 +0100

    fix(migrations): corrected migration code

--- internal/storage/postgres/migrations/20251127000004_drop_langfuse_column.sql
+++ internal/storage/postgres/migrations/20251127000004_drop_langfuse_column.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
--- +goose StatementEnd
 ALTER TABLE projects DROP COLUMN langfuse_secret_key_hash;
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE projects ADD COLUMN langfuse_secret_key_hash VARCHAR(255);
